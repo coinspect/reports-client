48bd50d3044bfee82c99a306eb95d1cc81978616
emi <emiliorizzo@gmail.com>
2022-09-19 21:35:24 +0000

    Update exports, add 'createApi'

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { dbApi as api } from './dbApi'
+export { createApi } from './dbApi'
--- src/test/exports.test.ts
+++ src/test/exports.test.ts
@@ -3,5 +3,6 @@ import * as x from '../index'
 describe('Exports', () => {
   test('api should be exported', () => {
     expect(typeof x.api).toBe('function')
+    expect(typeof x.createApi).toBe('function')
   })
 })
