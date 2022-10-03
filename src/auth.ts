821fca7ae25e8167ddd159c9ac74e692a0c060ea
emi <emiliorizzo@gmail.com>
2022-10-03 13:25:42 +0000

    Update auth, renew token
    see: vscode-coinspect-reports#21

--- src/auth.ts
+++ src/auth.ts
@@ -2,22 +2,78 @@ import {
   getAuth,
   signInWithCredential,
   GoogleAuthProvider,
-  reauthenticateWithCredential
+  UserCredential
 } from 'firebase/auth'
 import { FirebaseApp } from 'firebase/app'
+import fetch from 'node-fetch'
+
+const REFRESH_URL = 'https://securetoken.googleapis.com/v1/token'
+
+const getRefreshURL = (apiKey: string) => `${REFRESH_URL}?key=${apiKey}`
+
+type TokenResponse = {
+  idToken: string
+  refreshToken: string
+}
+
+const createRequest = (data: {}) => {
+  const method = 'POST'
+  const body = JSON.stringify(data)
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const headers = { 'Content-Type': 'application/json' }
+  return { method, body, headers }
+}
+
+export const refreshIdToken = async (
+  refreshToken: string,
+  apiKey: string
+): Promise<TokenResponse> => {
+  try {
+    const response = await fetch(
+      getRefreshURL(apiKey),
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      createRequest({ refreshToken, apiKey, grantType: 'refresh_token' })
+    )
+    const data = await response.json()
+    return { idToken: data.id_token, refreshToken: data.refresh_token }
+  } catch (err) {
+    return Promise.reject(err)
+  }
+}
+
+export const getUserDataFromCredential = async (credential: UserCredential) => {
+  const { user } = credential
+  const { email, displayName: name, refreshToken } = user
+  const idToken = await user.getIdToken(true)
+  return { email: `${email}`, name: `${name}`, idToken, refreshToken }
+}
 
 export const singInWithIdToken = async (
   app: FirebaseApp,
-  idToken: any,
-  accessToken: any
-) => {
+  idToken: string,
+  refreshToken?: string | undefined,
+  tries?: number | undefined
+): Promise<UserCredential> => {
   try {
+    if (tries === undefined) {
+      tries = 1
+    }
     const credential = GoogleAuthProvider.credential(idToken)
     const auth = getAuth(app)
     const result = await signInWithCredential(auth, credential)
-    console.log({ credential, result })
     return result
   } catch (err) {
+    const { apiKey } = app.options
+    // TODO check if is an idToken error
+    if (refreshToken && tries && apiKey) {
+      const tokens = await refreshIdToken(refreshToken, apiKey)
+      return singInWithIdToken(
+        app,
+        tokens.idToken,
+        tokens.refreshToken,
+        --tries
+      )
+    }
     // TODO Handle errors
     return Promise.reject(err)
   }
--- src/test/auth.test.ts
+++ src/test/auth.test.ts
@@ -1,17 +1,52 @@
 import { connectAuthEmulator, getAuth } from 'firebase/auth'
-import { projectId, authPort, apiKey, host } from './test-config'
-import { createApp } from '../dbApi'
-import { singInWithIdToken } from '../auth'
+import { host, app, authPort, idToken, apiKey } from './test-config'
+import * as a from '../auth'
+import { UserData } from '../dbApi'
 
-describe('auth', () => {
-  const app = createApp({ projectId, apiKey })
-  const auth = getAuth(app)
-  connectAuthEmulator(auth, `http://${host}:${authPort}`)
+export function testUserData(idToken: any, result: any) {
+  const idTokenData = JSON.parse(idToken)
+  expect(result.name).toBe(idTokenData.name)
+  expect(result.email).toBe(idTokenData.email)
+  expect(result.idToken).toBeDefined()
+  expect(result.refreshToken).toBeDefined()
+}
+
+const auth = getAuth(app)
+connectAuthEmulator(auth, `http://${host}:${authPort}`)
+
+afterEach(() => {
+  // restore the spy created with spyOn
+  jest.restoreAllMocks()
+})
+
+describe('singInWithIdToken', () => {
   it('should signin using an idToken', async () => {
-    const idToken =
-      '{"sub": "abc123", "email": "foo@example.com", "email_verified": true}'
-    const result = await singInWithIdToken(app, idToken)
+    const result = await a.singInWithIdToken(app, idToken)
     expect(result).not.toBeUndefined()
     expect(typeof result).toBe('object')
+  })
+})
+
+describe('getUserDataFromCredential', () => {
+  it('should get user data from credential', async () => {
+    const credential = await a.singInWithIdToken(app, idToken)
+    const result = await a.getUserDataFromCredential(credential)
+    testUserData(idToken, result)
+  })
+})
+
+describe('Renew idToken', () => {
+  it('should try to renew idToken', async () => {
+    const credential = await a.singInWithIdToken(app, idToken)
+    const { refreshToken } = await a.getUserDataFromCredential(credential)
+    const spy = jest
+      .spyOn(a, 'refreshIdToken')
+      .mockReturnValue(Promise.resolve({ idToken: '', refreshToken }))
+    const tries = 3
+    try {
+      const result = await a.singInWithIdToken(app, '', refreshToken, tries)
+    } catch (error) {
+      expect(spy).toHaveBeenCalledTimes(tries)
+    }
   })
 })
