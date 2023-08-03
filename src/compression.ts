56ba34e2b9777573dd4a7dc443a440b295191228
leoxs22 <leandro.narosky@gmail.com>
2023-08-03 11:18:54 -0300

    Avoid using promisify

--- src/compression.ts
+++ src/compression.ts
@@ -1,30 +1,35 @@
 import * as zlib from 'zlib'
-import { promisify } from 'util'
-const gunzip = promisify(zlib.gunzip)
-const gzip = promisify(zlib.gzip)
 
-export const decompressArrayBuffer = async (
-  arrayBuffer: ArrayBuffer
-): Promise<ArrayBuffer> => {
-  // Convert ArrayBuffer to Buffer
-  const buffer = Buffer.from(arrayBuffer)
+export const decompressArrayBuffer = (arrayBuffer: ArrayBuffer): Promise<ArrayBuffer> => {
+  return new Promise((resolve, reject) => {
+    // Convert ArrayBuffer to Buffer
+    const buffer = Buffer.from(arrayBuffer)
 
-  // Decompress the Buffer using 'zlib'
-  const decompressedBuffer = await gunzip(buffer)
-
-  // Convert the decompressed Buffer back to ArrayBuffer and return it
-  return Uint8Array.from(decompressedBuffer).buffer
+    // Decompress the Buffer using 'zlib'
+    zlib.gunzip(buffer, (err, decompressedBuffer) => {
+      if (err) {
+        reject(err)
+      } else {
+        // Convert the decompressed Buffer back to ArrayBuffer and return it
+        resolve(Uint8Array.from(decompressedBuffer).buffer)
+      }
+    })
+  })
 }
 
-export const compressArrayBuffer = async (
-  arrayBuffer: ArrayBuffer
-): Promise<Uint8Array> => {
-  // Convert ArrayBuffer to Buffer
-  const buffer = Buffer.from(arrayBuffer)
+export const compressArrayBuffer = (arrayBuffer: ArrayBuffer): Promise<Uint8Array> => {
+  return new Promise((resolve, reject) => {
+    // Convert ArrayBuffer to Buffer
+    const buffer = Buffer.from(arrayBuffer)
 
-  // Compress the Buffer using 'zlib'
-  const compressedBuffer = await gzip(buffer)
-
-  // Convert the compressed Buffer back to ArrayBuffer and return it
-  return Uint8Array.from(compressedBuffer)
+    // Compress the Buffer using 'zlib'
+    zlib.gzip(buffer, (err, compressedBuffer) => {
+      if (err) {
+        reject(err)
+      } else {
+        // Convert the compressed Buffer back to ArrayBuffer and return it
+        resolve(Uint8Array.from(compressedBuffer))
+      }
+    })
+  })
 }
